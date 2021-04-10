01bbffaa2e1c9c4746928ce0783723b92bf3ae61
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-04-10 23:38:15 +0200

    Add endpoint to catch all links

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ app.use(express.static(path.join(__dirname, '/client')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/index.html'));
+  });
 
 app.listen(8000, () => {
     console.log('Server is running on port: 8000');
