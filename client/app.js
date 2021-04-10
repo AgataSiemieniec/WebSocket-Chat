abfe2cedf1266f6d6578f6cead1ac5c97fec758a
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-04-10 23:12:50 +0200

    Fix bugs: typos in app.js & complete index.html

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 
 const loginForm = document.getElementById('welcome-form');
-const messagesSection = document.getElementById('message-section');
+const messagesSection = document.getElementById('messages-section');
 const messagesList = document.getElementById('messages-list');
 const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
@@ -8,44 +8,45 @@ const messageContentInput = document.getElementById('message-content');
 
 let userName = '';
 
-const login = (e) => {
-    e.preventDefault();
+const login = event => {
+    event.preventDefault();
     if(!userNameInput.value){
         alert('Sign in, please!');
     } else {
-        userName = userNameInput.value;
+        userNameInput.value = userName;
         loginForm.classList.remove('show');
         messagesSection.classList.add('show');
-    };
+    }
 };
 
-const sendMessage = (e) => {
-    e.preventDefault();
-    if(!messageContentInpute.value){
+const sendMessage = event => {
+    event.preventDefault();
+    if(!messageContentInput.value){
         alert('Wright a message, please!');
     } else {
         addMessage(userName, messageContentInput.value);
-        messageContentInpute.value = '';
-    };    
+        messageContentInput.value = '';
+    } 
 };
 
 const addMessage = (author, content) => {
     const message = document.createElement('li');
-    message.classList.add('message, message--received');
-    if(author === userName){
-        message.classList.add('message--self');
-    };
-    message.innerHTML =`
-    <h3 class="message__author">${userName  === author ? 'You' : author}</h3>;
-    <div class="message__content">${content}</div>;
+    message.classList.add('message');
+    message.classList.add('message--received');
+    if(author === userName) message.classList.add('message--self');
+    message.innerHTML = `
+      <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+      <div class="message__content">
+        ${content}
+      </div>
     `;
     messagesList.appendChild(message);
 };
 
-loginForm.addEventListener('submit', e => {
-    login(e);
+loginForm.addEventListener('submit', event => {
+    login(event);
   });
   
-addMessageForm.addEventListener('submit', e => {
-    sendMessage(e);
+addMessageForm.addEventListener('submit', event => {
+    sendMessage(event);
 });
